9cda757b3feadba914d5caa83646393eec189a94
loxknew <bangbang12353@gmail.com>
2023-07-09 19:23:24 +0700

    authentication with html for testing

---
+++ src/main/java/Authentication/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package Authentication.repository;
+
+import Authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
---
+++ src/main/java/Authentication/service/UserService.java
@@ -0,0 +1,13 @@
+package Authentication.service;
+
+import Authentication.dto.UserDto;
+import Authentication.entity.User;
+import java.util.List;
+
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    User findUserByEmail(String email);
+
+    List<UserDto> findAllUsers();
+}
