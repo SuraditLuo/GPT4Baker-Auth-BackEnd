2012dcc6c303c7ef8f4cecb15b931f614a40e565
Suradit Luo <suradit_luo@cmu.ac.th>
2023-10-03 20:53:11 +0700

    Make to production

--- src/test/java/com/example/gpt4bakerauthbackend/GPT4BakerApplicationTests.java
+++ src/test/java/com/example/gpt4bakerauthbackend/GPT4BakerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.gpt4bakerauthbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GPT4BakerApplicationTests {
 
 	@Test
 	void contextLoads() {
